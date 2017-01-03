95427462ad53dc74b4085b707361476e96cbe78c
Carsten Paasch <carsten.paasch@obi.de>
2017-01-03 08:05:23 +0100

    Initial commit

---
+++ src/PackageA/PackageA.Test.Unit/ClassOfPackageATest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace PackageA.Test.Unit
+{
+
+    [TestFixture]
+    public class ProgrammTest
+    {
+        [Test]
+        public void GetName_NameIsNotEmpty()
+        {
+            var subject = new ClassOfPackageA();
+            Assert.IsNotNullOrEmpty(subject.GetName());
+        }
+    }
+}
---
+++ src/PackageA/PackageA/ClassOfPackageA.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackageA
+{
+    public class ClassOfPackageA
+    {
+        public string GetName()
+        {
+            return "PackageA";
+        }
+
+        public string GetVersion()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var fileVersionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
+            string productVersion = fileVersionInfo.ProductVersion;
+            return productVersion;
+        }
+
+        public string GetSummary()
+        {            
+            return $"{GetName()} v. {GetVersion()}";
+        }
+    }
+}
