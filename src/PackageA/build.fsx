51ab17d1b6c802453cb87c4072b4d1068f6c0290
Alexander Groß <agross@therightstuff.de>
2017-01-10 14:24:57 +0100

    Split build and test targets

--- src/PackageA/build.fsx
+++ src/PackageA/build.fsx
@@ -29,7 +29,7 @@ let PrintYellow infoMessage =
 // -------------------------------------------------------------------------------------------------------------------------------------------------
 
 Description "Clean the target directory"
-Target "Clean" (fun _ -> 
+Target "Clean" (fun _ ->
     CleanDir targetDir
 )
 
@@ -37,13 +37,22 @@ Target "Clean" (fun _ ->
 
 Description "Compiles the project file"
 Target "Build" (fun _ ->
+    run "Clean"
 
-    MSBuildHelper.build(fun x -> 
-        { x  with 
+    MSBuildHelper.build(fun x ->
+        { x  with
             Verbosity = Some Minimal
             Properties =["OutputPath", "../" @@ buildDir; "Configuration", getBuildParamOrDefault "buildMode" "Debug"]
             NodeReuse = false}) projectFile
-    |> ignore    
+    |> ignore
+
+    MSBuildHelper.build(fun x ->
+        { x  with
+            Verbosity = Some Minimal
+            Properties =["OutputPath", "../" @@ testBuildDir; "Configuration", getBuildParamOrDefault "buildMode" "Debug"]
+            NodeReuse = false}) testProjectFile
+    |> ignore
+
 )
 
 // -------------------------------------------------------------------------------------------------------------------------------------------------
@@ -52,31 +61,24 @@ Target "BuildNuGetPackage" (fun _ ->
 
     let infoMessage = sprintf "Creating nuGet-Package: %s.%s.nupkg\n\n" packageName version
     PrintYellow infoMessage
-    
+
     CreateDir nugetInputFilesDir
     CopyFiles (nugetInputFilesDir @@ "lib" @@ "net45") !!(buildDir + "PackageA.*")
     CreateDir nugetOutputFilesDir
 
-    NuGet (fun p -> 
+    NuGet (fun p ->
         { p with
             WorkingDir = nugetInputFilesDir
             Version = version
             OutputPath = nugetOutputFilesDir
             Title = packageName
             Project = packageName
-        }) nuspecFile     
+        }) nuspecFile
 )
 
 // -------------------------------------------------------------------------------------------------------------------------------------------------
 Description "Run unit tests without category 'LongRunning'"
 Target "Test" (fun _ ->
-    MSBuildHelper.build(fun x -> 
-        { x  with 
-            Verbosity = Some Minimal
-            Properties =["OutputPath", "../" @@ testBuildDir; "Configuration", getBuildParamOrDefault "buildMode" "Debug"]
-            NodeReuse = false}) testProjectFile
-    |> ignore                
-    
     !!(testBuildDir @@ "**/*.Test.Unit.dll")
       |> NUnit (fun p ->
           {p with
@@ -93,11 +95,11 @@ let listTargets() =
     Console.ForegroundColor <- ConsoleColor.Green
     printf "Available targets:\n"
     TargetDict.Values
-      |> Seq.iter (fun target -> 
+      |> Seq.iter (fun target ->
             Console.ForegroundColor <- ConsoleColor.Yellow
             printf " %s\n" target.Name
             Console.ForegroundColor <- ConsoleColor.Gray
-            printf " %s\n" (if target.Description <> null then target.Description + "\n" else "")            
+            printf " %s\n" (if target.Description <> null then target.Description + "\n" else "")
             )
     Console.ForegroundColor <- curColor
 
@@ -109,13 +111,6 @@ Target "help" (fun _ ->
 
     listTargets()
 )
-
-// -------------------------------------------------------------------------------------------------------------------------------------------------
-
-"Clean"
-==> "Test"
-==> "Build"
-==> "BuildNuGetPackage"
 
 // -------------------------------------------------------------------------------------------------------------------------------------------------
 RunTargetOrDefault "help"
